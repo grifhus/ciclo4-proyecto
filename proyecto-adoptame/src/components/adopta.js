6cde3145e0a7ea9827ec8bdab6d245c300b0479f
David Julian Jaramillo <davidjulianjaramillo@gmail.com>
2022-11-30 22:17:44 -0500

    ok

--- proyecto-adoptame/src/components/adopta.js
+++ proyecto-adoptame/src/components/adopta.js
@@ -19,19 +19,7 @@ const Adopta = () => {
   
       {card.map((product) => (
         <div key={product.id}>
-          {/* <div>
-              <img
-                alt={product.Tipo}
-                src={product.img}
-                
-                width="100"
-                height="100"         
-              />
-            </div>
-            <div>
-              <h3>{product.nombre}</h3>
-              <p>{product.observacion}</p>
-            </div> */}
+
           <Card className="box-shadow" style={{ width: "18rem" }} data-aos="flip-down">
 
             <Card.Img
